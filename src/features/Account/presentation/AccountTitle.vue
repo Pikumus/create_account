adefff32f0b0ee81f2e8f6122f68b75f8d191052
Pikumus <Pikumus@gmail.com>
2025-05-17 10:35:58 +0300

    добавил tailwind css

---
+++ src/features/Account/presentation/AccountTitle.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+defineProps<{
+  title: string
+}>()
+</script>
+
+<template>
+  <h1 class="border bg-sky-500">{{ title }}</h1>
+</template>
+
+<style scoped></style>
---
+++ src/views/CreateAccountView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+import AccountTitle from '@/features/Account/presentation/AccountTitle.vue'
+</script>
+
+<template>
+<AccountTitle title="Создание аккаунта"/>
+</template>
+
+<style scoped>
+
+</style>
